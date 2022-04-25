2cbfb887ab576df4ee0d056e3d37e1056271b37e
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-04-25 08:50:33 +0530

    Initial Commit

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCt6AhVuGU93HeXQx5X_Ya_HhvMkKN5Cpo",
+  authDomain: "blogging-website-738df.firebaseapp.com",
+  projectId: "blogging-website-738df",
+  storageBucket: "blogging-website-738df.appspot.com",
+  messagingSenderId: "485195997654",
+  appId: "1:485195997654:web:e9f702d6b6e0079c1d4477",
+};
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
